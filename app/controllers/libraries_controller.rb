e8ea7145e7f496b9e1f292f672665e5888146464
It Ddd <itddd.julius@gmail.com>
2019-12-10 12:53:57 +0000

    JO - TUE10DEC - 2019
    libraries_controller.rb - modified

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,13 +1,41 @@
 class LibrariesController < ApplicationController
   def show
     @assets = Asset.where(library_id: current_library.id)
+    if params[:sort] && params[:sort].eql?("title_desc")
+      @assets = Asset.order("lower(filename) DESC")
+    end
+    if params[:sort] && params[:sort].eql?("title_asc")
+      @assets = Asset.order("lower(filename) ASC")
+    end
+    if params[:sort] && params[:sort].eql?("created_at_desc")
+      @assets = Asset.order("created_at DESC")
+    end
+    if params[:sort] && params[:sort].eql?("created_at_asc")
+      @assets = Asset.order("created_at ASC")
+    end
+    if params[:filter] && params[:filter].eql?("video")
+      @assets = Asset.where(file_type: 0)
+    end
+    if params[:filter] && params[:filter].eql?("image")
+      @assets = Asset.where(file_type: 1)
+    end
+    if params[:filter] && params[:filter].eql?("audio")
+      @assets = Asset.where(file_type: 2)
+    end
   end
 
   def index
-    if params[:sort] && params[:sort].eql?("alphabetical")
-      @libraries = Library.order("lower(name) DESC")
-    else
-      @libraries = Library.order("lower(name) ASC")
+    if params[:sort] && params[:sort].eql?("title_desc")
+      @assets = Asset.order("lower(filename) DESC")
+    end
+    if params[:sort] && params[:sort].eql?("title_asc")
+      @assets = Asset.order("lower(filename) ASC")
+    end
+    if params[:sort] && params[:sort].eql?("created_at_desc")
+      @assets = Asset.order("created_at DESC")
+    end
+    if params[:sort] && params[:sort].eql?("created_at_asc")
+      @assets = Asset.order("created_at ASC")
     end
 
     if @libraries.size == 1
